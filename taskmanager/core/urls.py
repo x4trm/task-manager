315828a0ccc03acd2e28f636699527c60744b819
x4trm <damian.maza@wp.pl>
2023-05-04 13:44:53 +0200

    update navbar and templates

--- taskmanager/core/urls.py
+++ taskmanager/core/urls.py
@@ -2,11 +2,14 @@ from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views 
 from .forms import LoginForm
+from django.contrib.auth.views import LogoutView
+
 
 app_name='core'
 
 urlpatterns = [
     path('',views.index),
     path('signup/',views.signup,name='signup'),
-    path('login/',auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm),name='login')
+    path('login/',auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout')
 ]
